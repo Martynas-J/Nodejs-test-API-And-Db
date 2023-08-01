10b99a99a31c347101be3bddf4c26116f9111d1d
Martynas-J <Aurimhas@gmail.com>
2023-08-01 18:38:18 +0300

    v1.3

--- index.js
+++ index.js
@@ -3,15 +3,61 @@ const app = express();
 const port = 3000;
 const router = require('./src/student/routes');
 const cors = require("cors")
+const os = require('os');
+const readline = require('readline');
 
 app.use(express.json())
 
-app.use(cors);
-app.use('/api/v1/students', router);
-app.use('/api/v1/students/:id', router);
+app.use(cors());
+app.use('/', router);
+app.use('/:id', router);
 
-app.get("/", (req, res) => {
-    res.send("hello jon")
-})
+const operacineSistema = os.platform();
+console.log('Operacinė sistema:', operacineSistema);
+
+const procesorius = os.cpus();
+console.log('Procesorius:', procesorius);
+
+const laisvaAtmintis = os.freemem();
+console.log('Laisva atmintis (bytes):', laisvaAtmintis);
+
+const bendraAtmintis = os.totalmem();
+console.log('Bendra atmintis (bytes):', bendraAtmintis);
+
+const tinkloSasajos = os.networkInterfaces();
+Object.keys(tinkloSasajos).forEach((sasajosPavadinimas) => {
+    const sasaja = tinkloSasajos[sasajosPavadinimas];
+    console.log(`Tinklo sąsaja: ${sasajosPavadinimas}`);
+
+    sasaja.forEach((sasajosInfo) => {
+        if (sasajosInfo.family === 'IPv4') {
+            console.log(`IP adresas: ${sasajosInfo.address}`);
+            console.log(`Subnet maska: ${sasajosInfo.netmask}`);
+        }
+    });
+});
+const hostPavadinimas = os.hostname();
+console.log('Kompiuterio pavadinimas:', hostPavadinimas);
+
+const veikimoLaikas = os.uptime();
+console.log('Sistemos veikimo laikas (sekundėmis):', veikimoLaikas);
+
+const naudotojoInfo = os.userInfo();
+console.log('Naudotojo informacija:', naudotojoInfo);
+
+
+const osVersija = os.release();
+console.log('Operacinės sistemos versija:', osVersija);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Pateikiame klausimą vartotojui ir laukiame jo įvesties
+rl.question('Koks jūsų vardas? ', (vardas) => {
+    console.log(`Sveiki, ${vardas}!`);
+    rl.close(); // Užbaigiame skaitymo interfeisą
+});
 
 app.listen(port, () => console.log(`app listening on port ${port}`)) 
